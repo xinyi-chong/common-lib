305fbb55785fb51f34420de63aa91781b14b5fe7
xinyi-chong <xinyi.chong.dev@gmail.com>
2025-09-06 22:59:08 +0800

    feat(consts): add context keys and standardized user header constants

--- consts/consts.go
+++ consts/consts.go
@@ -3,3 +3,18 @@ package consts
 const (
 	Localizer = "localizer"
 )
+
+// Context keys used in Gin/Go contexts
+const (
+	CtxAccessToken = "access_token"
+	CtxUserID      = "user_id"
+	CtxUsername    = "username"
+	CtxUserEmail   = "email"
+)
+
+// Standardized headers for forwarding user claims to downstream services
+const (
+	HeaderUserID    = "X-User-Id"
+	HeaderUsername  = "X-User-Name"
+	HeaderUserEmail = "X-User-Email"
+)
